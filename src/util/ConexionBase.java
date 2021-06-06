e8b8b7d4d50c8859f828a8ef5286bd91c0a09c1a
mollyvk <57828001+mollyvk@users.noreply.github.com>
2021-06-06 15:01:24 +0200

    JavaDoc e iméganes para readme agregadas

--- src/util/ConexionBase.java
+++ src/util/ConexionBase.java
@@ -22,10 +22,21 @@ import modelos.DiferenciasLinea;
 import modelos.Producto;
 import modelos.StockTeoricoLinea;
 
+/**
+ * 
+ * @author pablo
+ *
+ *         Esta clase es la utilizada para trabajar con la base de datos SQLite
+ *         que se ha creado en la aplicación.
+ */
 public class ConexionBase {
 	private static String url = "db/inventario.db";
 	private static Connection connector;
 
+	/**
+	 * Este método settea un objeto Connection con el que comunicar con la base de
+	 * datos.
+	 */
 	public static void connect() {
 		try {
 			connector = DriverManager.getConnection("jdbc:sqlite:" + url);
@@ -37,6 +48,9 @@ public class ConexionBase {
 		}
 	}
 
+	/**
+	 * Cierra la conexión con la base de datos.
+	 */
 	public static void close() {
 		try {
 			connector.close();
@@ -46,6 +60,14 @@ public class ConexionBase {
 		}
 	}
 
+	/**
+	 * Método que recibe un objeto de tipo linea y guarda sus propiedades en la
+	 * tabla "capturas" de la base de datos.
+	 * 
+	 * @param linea objeto con la información que debe guardarse.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static void insertarCaptura(Captura linea) throws SQLException {
 		String referencia = linea.getReferenciaCapturada();
 		int cantidad = linea.getCantidadCapturada();
@@ -62,6 +84,16 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Recupera un objeto Producto con la información almacenada el registro con el
+	 * último id de la tabla "capturas" de la base de datos.
+	 * 
+	 * @return objeto Producto con las propiedades del último registro insertado en
+	 *         la BBDD.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static Producto recuperarUltimaCaptura() throws SQLException {
 
 		Statement sentencia = connector.createStatement();
@@ -103,6 +135,20 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * Este método inserta registro a registro la información de cada linea de un
+	 * fichero obtenido.
+	 * 
+	 * @param stage Recibe la escena en la que estamos trabajo para poder lanzar un
+	 *              diálogo. TODO debería recibir una ArrayList del controlador y
+	 *              así no hay nada de vista en este componente.
+	 * @return un ArrayList de registros con toda la información que se ha cargado
+	 *         en la base.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 * @throws IOException  propaga una exepción si hay problemas con el acceso al
+	 *                      disco.
+	 */
 	public static ArrayList<BaseArticulosLinea> getBaseArticulos(Stage stage) throws SQLException, IOException {
 
 		ArrayList<BaseArticulosLinea> list = new ArrayList<BaseArticulosLinea>();
@@ -159,12 +205,28 @@ public class ConexionBase {
 		return list;
 	}
 
+	/**
+	 * 
+	 * Método que elimina todos los registros de la base de artículos.
+	 * 
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static void borrarBaseArticulos() throws SQLException {
 		Statement st = connector.createStatement();
 		st.executeUpdate("DELETE FROM baseProductos");
 
 	}
 
+	/**
+	 * 
+	 * Método que devuelve una ArrayList con la información de todos los registros
+	 * que se encuetran en la base de artículos.
+	 * 
+	 * @return ArrayList con información de la BBDD
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static ArrayList<BaseArticulosLinea> actualizarBaseArticulos() throws SQLException {
 
 		ArrayList<BaseArticulosLinea> base = new ArrayList<BaseArticulosLinea>();
@@ -205,6 +267,14 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Método que devuelve la información de la tabla stock_teórico de la BBDD
+	 * 
+	 * @return ArrayList con la información.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static ArrayList<StockTeoricoLinea> actualizarStockTeorico() throws SQLException {
 
 		ArrayList<StockTeoricoLinea> stockTeorico = new ArrayList<StockTeoricoLinea>();
@@ -226,12 +296,32 @@ public class ConexionBase {
 		return stockTeorico;
 	}
 
+	/**
+	 * Elimina los registros de la tabla stock_teorico de la BBDD
+	 * 
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static void borrarStockTeorico() throws SQLException {
 		Statement st = connector.createStatement();
 		st.executeUpdate("DELETE FROM stock_teorico");
 
 	}
 
+	/**
+	 * 
+	 * Guarda la información de un fichero con un formato de datos específico en la
+	 * tabla stock_teorico de la BBDD
+	 * 
+	 * @param stage recibe el stage en el que estamos trabajando. TODO estaría mejor
+	 *              recibir un ArrayList con la información y dejar el pedir el
+	 *              fichero al controlador de la vista.
+	 * @return ArrayList con la información de la tabla.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 * @throws IOException  propaga una excepción si hay un problema al acceder al
+	 *                      disco.
+	 */
 	public static ArrayList<StockTeoricoLinea> getStockTeorico(Stage stage) throws SQLException, IOException {
 		ArrayList<StockTeoricoLinea> list = new ArrayList<StockTeoricoLinea>();
 		// Pedir fichero
@@ -274,6 +364,18 @@ public class ConexionBase {
 		return list;
 	}
 
+	/**
+	 * 
+	 * Método que sirve para conocer el código interno de una referencia dada. La
+	 * referencia puede ser interna (7 u 8 dígitos) o EAN. Si tiene 7-8 se devuelve
+	 * la referencia ya que es interna y si no se busca en la base de datos como
+	 * EAN.
+	 * 
+	 * @param referenciaCapturada String con la referencia que queremos buscar.
+	 * @return String con la referencia interna del código enviado.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static String getCodigoInterno(String referenciaCapturada) throws SQLException {
 
 		Statement statement = connector.createStatement();
@@ -292,6 +394,16 @@ public class ConexionBase {
 		}
 
 	}
+
+	/**
+	 * 
+	 * Método que mediante una consulta SQL calcula la cantidad total de cantidades
+	 * capturadas de cada referencia auditada.
+	 * 
+	 * @return ArrayList con lo contado.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 
 	public static ArrayList<DiferenciasLinea> calcularContadoConsolidado() throws SQLException {
 
@@ -335,6 +447,14 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Carga la información de la tabla diferencias de la base de datos.
+	 * 
+	 * @return ArrayList con la información de la tabla diferencias.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static ArrayList<DiferenciasLinea> cargarDiferencias() throws SQLException {
 
 		ArrayList<DiferenciasLinea> list = new ArrayList<DiferenciasLinea>();
@@ -360,6 +480,15 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Busca si un código interno está o no en la base de datos.
+	 * 
+	 * @param referencia código interno que se quiere buscar.
+	 * @return true si existe en la base. false si no existe.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static boolean referenciaEnBase(String referencia) throws SQLException {
 
 		Statement st = connector.createStatement();
@@ -376,6 +505,14 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Busca la ubicación más alta que se ha contado.
+	 * 
+	 * @return int con el número más alto de ubiciación.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static int getLastLocation() throws SQLException {
 
 		Statement st = connector.createStatement();
@@ -387,6 +524,14 @@ public class ConexionBase {
 		return lastLocation;
 	}
 
+	/**
+	 * 
+	 * Muestra la cantidad de productos auditados en una ubicación.
+	 * 
+	 * @return entero con la cantidad de productos de una ubicación.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static int getQuantityLocation() throws SQLException {
 
 		Statement st = connector.createStatement();
@@ -400,6 +545,15 @@ public class ConexionBase {
 		return cantidad;
 	}
 
+	/**
+	 * 
+	 * Calcula las capturas que existen en una ubicación determinada.
+	 * 
+	 * @param ubicacion entero con la ubicación que se quiere consultar.
+	 * @return ArrayList con todas las captuas que existen en la ubicación.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static ArrayList<Captura> getCapturasUbicacion(int ubicacion) throws SQLException {
 
 		ArrayList<Captura> capturasUbicacion = new ArrayList<Captura>();
@@ -432,6 +586,16 @@ public class ConexionBase {
 		return capturasUbicacion;
 	}
 
+	/**
+	 * 
+	 * Busca en la BBDD la descripción de un código interno específico.
+	 * 
+	 * @param codigoInterno String con el código interno que se quiere buscar.
+	 * @return String con la descripción que aparece en la base de datos en el mismo
+	 *         registro que ese código interno.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	private static String getDescripcion(String codigoInterno) throws SQLException {
 		Statement st = connector.createStatement();
 
@@ -443,6 +607,15 @@ public class ConexionBase {
 
 	}
 
+	/**
+	 * 
+	 * Devuelve el código EAN de un código interno
+	 * 
+	 * @param codigoInterno String con el código interno.
+	 * @return String con el código EAN.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	private static String getCodigoEan(String codigoInterno) throws SQLException {
 		Statement st = connector.createStatement();
 
@@ -453,6 +626,14 @@ public class ConexionBase {
 		return ean;
 	}
 
+	/**
+	 * 
+	 * Elimina la captura con un id específico.
+	 * 
+	 * @param idSeleccionado int con el id de la captura que se quiere eliminar.
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
 	public static void eliminarLineaCaptura(int idSeleccionado) throws SQLException {
 
 		Statement st = connector.createStatement();
@@ -461,12 +642,22 @@ public class ConexionBase {
 
 	}
 
-	public static void actualizarLineaCaptura(int idSeleccionado, String nuevoCodigo, int nuevaCantidad, String nuevoCodigoInterno)
-			throws SQLException {
+	/**
+	 * 
+	 * Actualiza la información de un registro de la tabla capturas de la base de datos. 
+	 * 
+	 * @param idSeleccionado int con el id de la captura.
+	 * @param nuevoCodigo String con el nuevo código EAN
+	 * @param nuevaCantidad int con la nueva cantidad
+	 * @param nuevoCodigoInterno String con el nuevo código interno
+	 * @throws SQLException propaga una excepción si hay un problema al guardar la
+	 *                      información en la base de datos.
+	 */
+	public static void actualizarLineaCaptura(int idSeleccionado, String nuevoCodigo, int nuevaCantidad,
+			String nuevoCodigoInterno) throws SQLException {
 		Statement st = connector.createStatement();
 		st.executeUpdate("UPDATE capturas SET referencia_capturada = '" + nuevoCodigo + "', cantidad_capturada = "
-				+ nuevaCantidad + ", codigoInterno = '"
-						+ nuevoCodigoInterno +"' WHERE id = " + idSeleccionado + ";");
+				+ nuevaCantidad + ", codigoInterno = '" + nuevoCodigoInterno + "' WHERE id = " + idSeleccionado + ";");
 
 	}
 
